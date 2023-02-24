0868765655bcf827d8293c7ab8b5b84ab03159f8
LucasCorrea <lucascorrea1505@gmail.com>
2023-02-24 12:28:48 -0300

    Adicionando Models

---
+++ Empreguei/Models/Usuario.cs
@@ -0,0 +1,13 @@
+namespace Empreguei.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Genero { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Senha { get; set; }
+        
+    }
+}
